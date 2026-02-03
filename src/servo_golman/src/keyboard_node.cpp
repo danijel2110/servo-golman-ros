e72ef52be9420f6771403dfa92eeecf4f864ec59
Danijel Markovic <danijel.m2110@gmail.com>
2026-02-03 14:14:39 +0100

    Initial commit - ROS servo golman project

---
+++ src/servo_golman/src/keyboard_node.cpp
@@ -0,0 +1,53 @@
+#include <ros/ros.h>
+#include <std_msgs/Int32.h>
+#include <termios.h>
+#include <unistd.h>
+#include <stdio.h>
+
+char getKey()
+{
+    char c;
+    struct termios oldt, newt;
+
+    tcgetattr(STDIN_FILENO, &oldt);
+    newt = oldt;
+    newt.c_lflag &= ~(ICANON | ECHO);
+    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+
+    read(STDIN_FILENO, &c, 1);
+
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+    return c;
+}
+
+int main(int argc, char** argv)
+{
+    ros::init(argc, argv, "keyboard_node");
+    ros::NodeHandle nh;
+
+    ros::Publisher pub = nh.advertise<std_msgs::Int32>("/servo_angle", 10);
+
+    int angle = 90;
+    ROS_INFO("A/D pomeranje | Q izlaz");
+
+    while (ros::ok())
+    {
+        char key = getKey();
+
+        if (key == 'a')
+            angle = std::min(angle + 5, 180);
+        else if (key == 'd')
+            angle = std::max(angle - 5, 0);
+        else if (key == 'q')
+            break;
+
+        std_msgs::Int32 msg;
+        msg.data = angle;
+        pub.publish(msg);
+
+        ROS_INFO("Ugao: %d", angle);
+    }
+
+    return 0;
+}
+
